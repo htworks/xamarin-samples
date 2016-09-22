854a6e326a99dca6c2343bf8a880fbb5b4e4a48e
htworks <hideokohama@gmail.com>
2016-09-22 23:15:44 +0900

    GoogleMap
    GoogleMap表示サンプル

---
+++ SampleGoogleMap/SampleGoogleMap/SampleGoogleMap.iOS/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace SampleGoogleMap.iOS
+{
+
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            Xamarin.FormsGoogleMaps.Init("アクセスキー"); //追加
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
